9f6eccc20b7cac0d559f17ba0fb228c5b947bdf6
rawi123 <rawipro.rl@gmail.com>
2021-12-08 10:45:36 +0200

    process env

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use("/user", require('./routers/userRouter'));
 app.use("/pokemon", require('./routers/pokemonRouter'));
 
-mongoose.connect(`mongodb+srv://rawi:rawi123@cluster0.exbid.mongodb.net/monfensse?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGOOSE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(data => {
         app.listen(process.env.PORT || 4000);
         console.log("connected to db port 4000");
